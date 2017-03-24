96ab0bc336b860457a890bb95b3276619afa7acb
tihandjan <tihandjan198921@gmail.com>
2017-03-24 06:58:12 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, default: {format: :json} do 
+    mount_devise_token_auth_for 'Admin', at: 'admin_auth'
+  end
 end
