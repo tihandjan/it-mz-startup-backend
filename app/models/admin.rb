7757353e3efce7433c3a67e30f2dd15b1f29b0c7
tihandjan <tihandjan198921@gmail.com>
2017-03-25 07:35:03 +0300

    update admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable,
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
