3ea05da60a2fd21147d8fb21c0e833cd514f4762
tihandjan <tihandjan198921@gmail.com>
2017-08-03 04:10:16 +0300

    ..

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -61,8 +61,8 @@ class Api::V1::RecipesController < ApplicationController
 
     def recipe_params
       recipe = params.require(:recipe).permit(:title, :image, :summary, :time, :porsion, :complexity, :publish, :category_id)
-      recipe[:sub_category_id] = params[:sub_category_id] unless params[:sub_category_id].to_i == 0
-      recipe[:country_id] = params[:country_id] unless params[:country_id] == 'Кухня какой страны'
+      recipe[:sub_category_id] = params[:sub_category_id] unless SubCategory.find_by_id(params[:sub_category_id]).blank?
+      recipe[:country_id] = params[:country_id] unless Country.find_by_id(params[:country_id]).blank?
       recipe
     end
 
